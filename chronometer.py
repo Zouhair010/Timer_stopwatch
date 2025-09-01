3fd16d192d8d1b8e4e1e02333f994b434084f089
Zouhair010 <zouhairouazza@gmail.com>
2025-09-01 22:48:06 +0100

    change the time format to display hours and minutes correctly by using the choronometerFormat method

--- chronometer.py
+++ chronometer.py
@@ -3,12 +3,11 @@ from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.gridlayout import MDGridLayout
 from kivy.uix.textinput import TextInput
-from kivymd.uix.list import OneLineListItem, MDList, TwoLineListItem
+from kivymd.uix.list import OneLineListItem, MDList
 from kivymd.uix.scrollview import MDScrollView
 from kivymd.uix.widget import Widget
 from kivymd.uix.button import MDRectangleFlatIconButton
 from threading import Thread
-import threading
 from kivymd.color_definitions import colors
 from kivy.clock import Clock
 import time
@@ -24,15 +23,15 @@ class FirstScreen(MDBoxLayout):
         self.padding = 20
         self.spacing = 10
 
-        # Box for timer display
-        self.timer_boxlayout = MDBoxLayout(
+        # Box for chronometer display
+        self.chronometer_boxlayout = MDBoxLayout(
             orientation="horizontal",
             size_hint_y=None,
             height=60,
         )
 
-        # Timer text input (read-only)
-        self.timer_textInput = TextInput(
+        # chronometer text input (read-only)
+        self.chronometer_textInput = TextInput(
             halign="center",
             readonly=True,
             size_hint=(140,None),
@@ -44,11 +43,11 @@ class FirstScreen(MDBoxLayout):
             foreground_color=colors["Blue"]["800"]
         )
 
-        # Add timer with spacers
-        self.timer_boxlayout.add_widget(Widget(height=100)) # spacer
-        self.timer_boxlayout.add_widget(self.timer_textInput)
-        self.timer_boxlayout.add_widget(Widget(height=100)) # spacer
-        self.add_widget(self.timer_boxlayout)
+        # Add chronometer with spacers
+        self.chronometer_boxlayout.add_widget(Widget(height=100)) # spacer
+        self.chronometer_boxlayout.add_widget(self.chronometer_textInput)
+        self.chronometer_boxlayout.add_widget(Widget(height=100)) # spacer
+        self.add_widget(self.chronometer_boxlayout)
 
         # Layout for buttons
         self.buttons_boxlayout = MDGridLayout(cols=8, spacing=10, padding=20, size_hint_y=None, height=50)
@@ -108,103 +107,113 @@ class FirstScreen(MDBoxLayout):
         self.scroll.add_widget(self.laps)
         self.add_widget(self.scroll)
 
-        # Initialize timer display
-        self.replaceTime(f"{0:02}:{0:02}:{0:02}.{0:06}")
+        # Initialize chronometer display
+        self.replaceChronometerValue(f"{0:02}.{0:06}")
 
         # Condition for thread loop
         self.cond = True
         
-        #to check if there is a updatetime thread 
+        #to check if there is a update chronometer thread 
         self.therIsthread = False
 
-    # Start timer thread
+    # Start chronometer
     def start(self, instance):
-        #add a if statment to avoid creating many updatetime threads by checking if ther is a thread
+        #add a if statment to avoid creating many updateChronometerDisplay threads by checking if ther is a thread
         if not self.therIsthread:
             global thread
             self.therIsthread = True
-            thread = Thread(target=self.updateTime)
+            thread = Thread(target=self.updateChronometerDisplay)
             thread.daemon = True
             thread.start()
     
-    # update timer display using datetime 
-    def updateTime(self):
-        currrentDateTime = datetime.now()
-        #intialDateTime is the diffrence between the start datetime and the delta (the time on the stopwatch)
+    # update chronometer display using datetime 
+    def updateChronometerDisplay(self):
+        #intialDateTime is the diffrence between the start datetime and the delta (the time on the chronometer)
         # intialDateTime allows us to complete counting from the time that we was stoped in
-        h,m,s,ms = self.convertTime(self.getTime()) #the time on the stopwatch as (hours, minutes, seconds, microseconds)
+        currrentDateTime = datetime.now()
+        h,m,s,ms = self.convertChronometerValue(self.getChronometerValue()) #the time on the chronometer as (hours, minutes, seconds, microseconds)
         delta = timedelta(hours=h,minutes=m,seconds=s,microseconds=ms)
-        print(currrentDateTime)
         intialDateTime = currrentDateTime-delta
-        print(intialDateTime)
-        #  This relies on real time, making the timing more accurate and updating every 0.000001 seconds.
+        #  This relies on real time, making the chronometer more accurate and updating every 0.000001 seconds.
         while self.cond:
             currDateTime = datetime.now()
             # calculate the difference between the current datetime and the start datetime directly (currDateTime - intialDateTime).
-            Clock.schedule_once(lambda dt: self.replaceTime(f"{currDateTime-intialDateTime}"))
+            Clock.schedule_once(lambda dt: self.replaceChronometerValue(self.chronometerFormat(currDateTime-intialDateTime)))
             time.sleep(0.000001)
-
-    # Pause timer
+    
+    #change the chronometer format to display hours and minutes correctly
+    def chronometerFormat(self,chronoValue):
+        h,m,s,ms = self.convertChronometerValue(str(chronoValue))
+        if h > 0:
+            return str(chronoValue)
+        elif m > 0:
+            return f"{m:02}:{s:02}.{ms:06}"
+        else:
+            return f"{s:02}.{ms:06}"
+               
+    # Pause chronometer
     def pause(self, instance):
-        #to avoid NameError(name 'thread' is not defined.) if the pause button is pressed befor start button( befor creating updatetime thread)
+        #to avoid NameError(name 'thread' is not defined) if 
+        # the pause button is pressed befor start button( befor creating updateChronometer thread).
         if self.therIsthread:
             self.cond = False
             thread.join()
             self.cond = True
             self.therIsthread = False
 
-    # Restart timer
+    # Restart chronometer
     def restart(self, instance):
-        #to avoid NameError(name 'thread' is not defined.) if the restart button is pressed befor start button( befor creating updatetime thread)
+        #to avoid NameError(name 'thread' is not defined.) if 
+        # the restart button is pressed befor start button( befor creating updateChronometer thread)
         if self.therIsthread:
             self.cond = False
             thread.join()
-            self.replaceTime(f"{0:02}:{0:02}:{0:02}.{0:06}")
+            self.replaceChronometerValue(f"{0:02}.{0:06}")
             self.cond = True
             self.therIsthread = False
             self.laps.clear_widgets()
         else:
-            self.replaceTime(f"{0:02}:{0:02}:{0:02}.{0:06}")
+            self.replaceChronometerValue(f"{0:02}.{0:06}")
             self.laps.clear_widgets()
 
-    # Add current timer value as a lap on the scrollable lips list
+    # Add current chronometer value as a lap on the scrollable lips list
     def addLaps(self, instance):
-        #to avoid adding laps befor geting stopwatch started 
+        #to avoid adding laps befor geting the chronometer started 
         if self.therIsthread:
             self.laps.add_widget(OneLineListItem(
-                text=self.timer_textInput.text.strip(),
+                text=self.chronometer_textInput.text.strip(),
                 font_style='H6',
                 theme_text_color="Custom",
                 text_color=colors["Blue"]["800"],
             ))
 
-    # Replace displayed time
-    def replaceTime(self, txt):
-        self.timer_textInput.text = txt
+    # Replace displayed chronometer value
+    def replaceChronometerValue(self, txt):
+        self.chronometer_textInput.text = txt
             
+    # Get desplyed chronometer value as string
+    def getChronometerValue(self):
+        return self.chronometer_textInput.text.strip()
 
-    # Get desplyed timer value as string
-    def getTime(self):
-        return self.timer_textInput.text.strip()
-
-    # Convert time string into hours, minutes, seconds, microseconds
-    def convertTime(self, time):
-        durParts = []
+    # Convert chronometer value (string) into hours, minutes, seconds, microseconds
+    def convertChronometerValue(self, chronoValue):
+        timeParts = [0,0,0,0]
+        tracker = -1
         part = ""
-        for ch in time:
-            if ch.isdigit():
-                part += ch
+        for i in range(len(chronoValue)-1,-1,-1):
+            if chronoValue[i].isdigit():
+                part = chronoValue[i]+part
             else:
-                durParts.append(int(part))
+                timeParts[tracker] = int(part)
+                tracker -= 1
                 part = ""
-        durParts.append(int(part))
-        part = ""
-        return durParts
+        timeParts[tracker] = int(part)
+        return timeParts
 
 # Main app class
-class Timer(MDApp):
+class Chronometer(MDApp):
     def build(self):
         return FirstScreen()
 
 if __name__ == "__main__":
-    Timer().run()
+    Chronometer().run()
